e21182cf4b8dbe0c14bac973cf7c31b5bf569d90
brutumfulmen97 <vlatkooop@gmail.com>
2023-10-24 10:35:35 +0200

    ui pocetak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="text-red-300 text-4xl">
-            <h1 className="text-blue-500">Hello World</h1>
-            <p>Paragraph</p>
-            <Header />
-        </div>
+        <>
+            <div className="relative mt-12 mx-2">
+                <Image
+                    src="/coathanger2.png"
+                    alt="coat hanger"
+                    width={3000}
+                    height={1000}
+                    style={{ width: "100%", height: "auto" }}
+                />
+                <div className="absolute top-12 z-10 left-0 w-full bg-green-400 opacity-25 flex px-8">
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                    <div className="bg-green-300 w-full text-center">123</div>
+                </div>
+            </div>
+        </>
     );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 const Header = ({}) => {
-    return <div>Header</div>;
+    return (
+        <header>
+            <Image
+                src="/headerImage2022.png"
+                width={1000}
+                height={1000}
+                style={{ width: "100%", height: "auto" }}
+                priority={true}
+                alt="header image"
+            />
+        </header>
+    );
 };
 
 export default Header;
